ca004b00a022f83d103551d828be396b6c3af120
nathanhoyes <07nhoyes@gmail.com>
2023-02-13 15:40:59 +0000

    Added placeholder print to communicate that clicking on cells doesn't work when animation is playing

--- src/CellClickListener.java
+++ src/CellClickListener.java
@@ -12,7 +12,7 @@ public class CellClickListener extends MouseAdapter {
 
     @Override
     public void mouseClicked(MouseEvent event) {
-        System.out.printf("Mouse event registered, at %s, %s which is cell at %s, %s. Animation is %s.%n", event.getX(), event.getY(), event.getX() / gol.getCellSize(), event.getY() / gol.getCellSize(), animate.isRunning()?"playing":"paused");
+        //System.out.printf("Mouse event registered, at %s, %s which is cell at %s, %s. Animation is %s.%n", event.getX(), event.getY(), event.getX() / gol.getCellSize(), event.getY() / gol.getCellSize(), animate.isRunning()?"playing":"paused");
         if (!animate.isRunning()) {
             int x = event.getX() / gol.getCellSize();
             int y = event.getY() / gol.getCellSize();
@@ -20,6 +20,8 @@ public class CellClickListener extends MouseAdapter {
             System.out.printf("Changing cell at %s, %s.%n", x,y);
             cell.changeAlive();
             gol.repaint();
+        } else {
+            System.out.println("Animation running, pause to interact with cells.");
         }
     }
 }
